691404ec0a0902629419775f09e3ae5cf2429565
Michael J. Dole <webmaster@mjduniverse.com>
2020-11-06 21:31:47 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+// Electron Module
+
+const { app, BrowserWindow, Menu } = require('electron');
+
+// Menu
+
+var template = [
+    
+]
+
+// Window creation
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('./src/index.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
